3a86ed3fdab6b1e05ca7046c7e57f34d4dad8680
Ayushkumar12 <badshah112003@gmail.com>
2025-09-05 18:02:05 +0530

    ok

--- server.js
+++ server.js
@@ -49,9 +49,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? ['https://your-domain.com'] 
-    : ['http://localhost:3000', 'http://localhost:5173'],
+  origin: "*",
   credentials: true
 }));
 
